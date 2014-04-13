95657081db05a2c818b34f062dba2dd9b6e70b19
kashomon <jrhoak@gmail.com>
2014-04-13 17:12:46 -0600

    Update to 10.7

--- src/global.js
+++ src/global.js
@@ -4,7 +4,7 @@ glift.global = {
    * See: http://semver.org/
    * Currently in alpha.
    */
-  version: '0.10.6',
+  version: '0.10.7',
   debugMode: false,
   // Options for performanceDebugLevel: NONE, INFO
   performanceDebugLevel: 'NONE',
