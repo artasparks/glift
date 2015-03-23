81c0bba0d8acd098e01ed550f55fed3c67fa57eb
kashomon <jrhoak@gmail.com>
2015-03-22 20:30:27 -0700

    Change the way marked is exposed so it's consistent between node and web.

--- src/markdown/marked.js
+++ src/markdown/marked.js
@@ -1259,13 +1259,7 @@ marked.inlineLexer = InlineLexer.output;
 
 marked.parse = marked;
 
-if (typeof module !== 'undefined' && typeof exports === 'object') {
-  module.exports = marked;
-} else if (typeof define === 'function' && define.amd) {
-  define(function() { return marked; });
-} else {
-  this.marked = marked;
-}
+this.marked = marked;
 
 }).call(function() {
   return glift;
