d1f6cb4900b8d0257c9ad9a821bc0549a7ea04fc
Josh Hoak <jrhoak@gmail.com>
2016-04-27 12:26:56 -0400

    Add typing to ajax.js

--- compiled/glift_combined.js
+++ compiled/glift_combined.js
@@ -1714,12 +1714,16 @@ goog.provide('glift.ajax');
  * Ajax/XHR wrapper.
  */
 glift.ajax = {
-  get: function(url, callback) {
+  /**
+   * @param {string} url
+   * @param {function(string)} successCallback
+   */
+  get: function(url, successCallback) {
     var request = new XMLHttpRequest();
     request.onreadystatechange = function() {
       if (request.readyState === 4) {
         if (request.status === 200 || request.status === 304) {
-          callback(request.responseText);
+          successCallback(request.responseText);
         } else {
           // We reached our target server, but it returned an error
           console.error('(' + request.status + ') Error retrieving ' + url);
--- src/ajax/ajax.js
+++ src/ajax/ajax.js
@@ -4,12 +4,16 @@ goog.provide('glift.ajax');
  * Ajax/XHR wrapper.
  */
 glift.ajax = {
-  get: function(url, callback) {
+  /**
+   * @param {string} url
+   * @param {function(string)} successCallback
+   */
+  get: function(url, successCallback) {
     var request = new XMLHttpRequest();
     request.onreadystatechange = function() {
       if (request.readyState === 4) {
         if (request.status === 200 || request.status === 304) {
-          callback(request.responseText);
+          successCallback(request.responseText);
         } else {
           // We reached our target server, but it returned an error
           console.error('(' + request.status + ') Error retrieving ' + url);
@@ -18,7 +22,7 @@ glift.ajax = {
     };
     request.onerror = function() {
       throw new Error(request.responseText);
-      // There was a connection error of some sort
+      // There was a connection error of some sort.
     };
     request.open('GET', url, true);
     request.send();
