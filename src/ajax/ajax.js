0994887a4729246b3842ee787e17107c7ed365a0
Josh Hoak <jrhoak@gmail.com>
2016-04-29 19:13:10 -0600

    Little more work on ajax as prep work for kgs/async work.

--- src/ajax/ajax.js
+++ src/ajax/ajax.js
@@ -7,21 +7,32 @@ glift.ajax = {
   /**
    * @param {string} url
    * @param {function(string)} successCallback
+   * @param {function(number, string)=} opt_failureCallback
    */
-  get: function(url, successCallback) {
+  get: function(url, successCallback, opt_failureCallback) {
     var request = new XMLHttpRequest();
     request.onreadystatechange = function() {
       if (request.readyState === 4) {
         if (request.status === 200 || request.status === 304) {
           successCallback(request.responseText);
         } else {
-          // We reached our target server, but it returned an error
-          console.error('(' + request.status + ') Error retrieving ' + url);
+          if (opt_failureCallback) {
+            opt_failureCallback(request.status, request.responseText);
+          } else {
+            // We reached our target server, but it returned an error
+            console.error('(' + request.status + ') Error retrieving ' + url);
+          }
         }
       }
     };
     request.onerror = function() {
-      throw new Error(request.responseText);
+      if (opt_failureCallback) {
+        opt_failureCallback(request.status, request.responseText);
+      } else {
+        // We reached our target server, but it returned an error
+        console.error('(' + request.status + ') Error retrieving ' + url 
+            + '. ' + request.responseText);
+      }
       // There was a connection error of some sort.
     };
     request.open('GET', url, true);
