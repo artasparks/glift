f23f76af6912c1686488c4d17f987068d8bff7c4
Josh Hoak <jrhoak@gmail.com>
2016-06-03 19:11:16 -0600

    fix build. goog.provide must be provided at the file-scope.

--- src/glift.js
+++ src/glift.js
@@ -7,19 +7,19 @@
  * --------------------------------------
  */
 
-
-(function(w) {
 // Define some closure primitives for compatibility with dev mode. Closure
 // compiler works off of regular expressions, so this shouldn't be an issue.
 // This allows us to use goog.require and goog.provides in dev mode.
-if (!w['goog']) {
-  w['goog'] = {}
-  w['goog']['require'] = function(ns){};
-  w['goog']['scope'] = function(fn) { fn() };
-  w['goog']['provide'] = function(ns) { };
+if (window && !window['goog']) {
+  window['goog'] = {}
+  window['goog']['require'] = function(ns){};
+  window['goog']['scope'] = function(fn) { fn() };
+  window['goog']['provide'] = function(ns) { };
 }
 
 goog.provide('glift');
+
+(function(w) {
 
 var glift = w.glift || {};
 if (w) {
