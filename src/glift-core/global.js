9cf514245176c067aa644966cf246497ca09ed66
Josh Hoak <jrhoak@gmail.com>
2016-06-10 09:21:19 -0600

    Move global to glift-core and modify it for glift-ui

---
+++ src/glift-core/global.js
@@ -0,0 +1,11 @@
+goog.provide('glift.global');
+
+glift.global = {
+  /**
+   * Semantic versioning of the core Glift rules/logic.
+   * See: http://semver.org/
+   *
+   * Not yet stable.
+   */
+  'core-version': '0.9.0'
+};
--- src/glift-ui/global.js
+++ src/glift-ui/global.js
@@ -1,33 +1,31 @@
-goog.provide('glift.global');
+goog.require('glift.global');
 
 /**
- * Useful global variables related to all glift instances on the page.
+ * Semantic versioning is used to determine API behavior. This is the version of
+ * the Glift UI (which most people think of as Glift, anyway.
+ *
+ * See: http://semver.org/
+ *
+ * Currently on stable.
  */
-glift.global = {
-  /**
-   * Semantic versioning is used to determine API behavior.
-   * See: http://semver.org/
-   * Currently on stable.
-   */
-  version: '1.1.1',
+glift.global.version = '1.1.1';
 
-  /**
-   * The registry.  Used to determine who has 'ownership' of key-presses.
-   * The problem is that key presses have to be captured in a global scope (or
-   * at least at the <body> level.
-   */
-  instanceRegistry: {
-    // Map of manager ID (some-div-id-glift-1) to object instance.
-  },
-
-  /**
-   * ID of the active Glift instance.
-   */
-  activeInstanceId: null,
-
-  /** Used to mark whether the zoom has been disabled (for mobile). */
-  disabledZoom: false,
-
-  /** Added CSS classes (we only want to do this once). */
-  addedCssClasses: false
+/**
+ * The registry.  Used to determine who has 'ownership' of key-presses.
+ * The problem is that key presses have to be captured in a global scope (or
+ * at least at the <body> level.
+ */
+glift.global.instanceRegistry = {
+  // Map of manager ID (some-div-id-glift-1) to object instance.
 };
+
+/**
+ * ID of the active Glift instance.
+ */
+glift.global.activeInstanceId = null;
+
+/** Used to mark whether the zoom has been disabled (for mobile). */
+glift.global.disabledZoom = false;
+
+/** Added CSS classes (we only want to do this once). */
+glift.global.addedCssClasses = false;
