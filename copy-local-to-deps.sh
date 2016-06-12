90f1a2b5882f72dc9bb777bd1a8ad0a9b389f87c
Josh Hoak <jrhoak@gmail.com>
2016-06-12 17:54:46 -0600

    Update copy-script with usage.

--- copy-local-to-deps.sh
+++ copy-local-to-deps.sh
@@ -2,38 +2,53 @@
 
 set -e
 
-readonly path=$@
+USAGE="=======================================================================
+  copy-local-to-deps.sh <path>
 
-if [[ -z path ]]; then
+  Copies a local node path to the deps and marks the directory read-only. This
+  assumes that all the sources lie within the src/ tree, so is of limited use
+  generally. However, it's useful for copying glift-core into the deps tree.
+
+  Eventually, this will support pulling directly from Github.
+
+  Example:
+      copy-local-to-deps.sh ../glift-core"
+
+readonly REPO_PATH=$@
+
+if [[ -z $REPO_PATH ]]; then
   echo "You must supply a path for the local repo!!"
+  echo "$USAGE" >&2
   exit 1
 fi
 
-if [[ ! -d $path ]]; then
+if [[ ! -d $REPO_PATH ]]; then
   echo "Path supplied was not a directory!"
+  echo "$USAGE" >&2
   exit 1
 fi
 
-if [[ ! -f ${path}package.json ]]; then
+if [[ ! -f ${REPO_PATH}package.json ]]; then
   echo "Could not find a package.json!"
+  echo "$USAGE" >&2
   exit 1
 fi
 
-
-readonly name=$(grep "^  \"name\":.*" ${path}package.json | sed "s/.*\"\(.*\)\"[^:].*/\1/g")
+readonly name=$(grep "^  \"name\":.*" ${REPO_PATH}package.json | sed "s/.*\"\(.*\)\"[^:].*/\1/g")
 
 if [[ -z $name ]]; then
   echo "Could not find the package.json!"
+  echo "$USAGE" >&2
   exit 1
 fi
 
-readonly src_path="${path}src"
+readonly SRC_PATH="${REPO_PATH}src"
 
-readonly lib_path=$(echo $0 | sed "s/\\/[^/]*$/\\//g")deps/$name
+readonly LIB_PATH=$(echo $0 | sed "s/\\/[^/]*$/\\//g")deps/$name
 
-echo "Copying $src_path to $lib_path"
+echo "Copying $SRC_PATH to $LIB_PATH"
 
 # Ensure that the dirs are readonly.
-rsync -r $src_path/* $lib_path
+rsync -r $SRC_PATH/* $LIB_PATH
 
-find $lib_path -name "*.js" -print | xargs chmod a-w
+find $LIB_PATH -name "*.js" -print | xargs chmod a-w
